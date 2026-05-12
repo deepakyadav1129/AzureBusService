7e067e177f4c67d4f88965fb1852ab523272ea0e
deepakyadav1129 <deepakyadav@1129@gmail.com>
2026-05-12 17:55:49 +0530

    Set-up Producer for sending the message to email-queue to azure

---
+++ Queue/Producer/Controllers/ServiceBusController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Producer.Services;
+using System.Text.Json;
+
+namespace Producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceBusController : ControllerBase
+    {
+        private readonly ServiceBusSenderService _serviceBusSenderService;
+        public ServiceBusController(ServiceBusSenderService serviceBusSenderService)
+        {
+            _serviceBusSenderService = serviceBusSenderService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage([FromBody] string data)
+        {
+            await _serviceBusSenderService.SendMessageAsync(data);
+            return Ok("Message sent to Service Bus");
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create()
+        {
+            var message = new
+            {
+                OrderId = Guid.NewGuid(),
+                Message = "Test Message"
+            };
+            await _serviceBusSenderService.SendMessageAsync(message);
+            return Ok("Message created and sent to Service Bus"); 
+        }
+    }
+}
---
+++ Queue/Producer/Services/ServiceBusSenderService.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text;
+using System.Text.Json;
+
+namespace Producer.Services
+{
+    public class ServiceBusSenderService
+    {
+        private readonly IConfiguration _configuration;
+        public ServiceBusSenderService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendMessageAsync(object data) //data {orderid:1, message:"Test Message"}
+        {
+            //var option = new JsonSerializerOptions
+            //{
+            //    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            //    PropertyNameCaseInsensitive = true,
+            //    WriteIndented = true
+            //};
+            var connectionString = _configuration["ServiceBus:ConnectionString"];
+            var queueName = _configuration["ServiceBus:QueueName"];
+            var client = new ServiceBusClient(connectionString);
+            var sender = client.CreateSender(queueName);
+
+            var json = JsonSerializer.Serialize(data);
+
+            var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(json));
+            await sender.SendMessageAsync(message);
+
+            await sender.DisposeAsync(); 
+            await client.DisposeAsync();
+        }
+    }
+}
